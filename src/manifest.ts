2ff3f977e23dc7ee790ea36a4f10f55ce7363509
snyt45 <snyt45@gmail.com>
2021-11-23 17:13:59 +0900

    manifest.ts修正

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,14 +1,17 @@
 export const getManifest = () => {
   return {
     manifest_version: 3,
-    name: "Google Meet Slack Emoji Reactions",
-    description: "Google MeetにSlackの絵文字資産を使ったリアクション機能を提供します。",
-    version: "1.0",
+    name: 'Google Meet Slack Emoji Reactions',
+    description: 'Google MeetにSlackの絵文字資産を使ったリアクション機能を提供します。',
+    version: '0.1',
     content_scripts: [
       {
         matches: ['http://meet.google.com/*', 'https://meet.google.com/*'],
         js: ['./dist/contentScripts/index.global.js'],
       },
     ],
+    action: {
+      'default_popup': './dist/popup/index.html'
+    },
   }
 }
