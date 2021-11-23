34b5adbf907fd7be0f3d822584a1ba1b7dee21e9
snyt45 <snyt45@gmail.com>
2021-11-23 17:14:19 +0900

    prepare.ts修正

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -2,7 +2,7 @@ import fs from 'fs-extra'
 import { getManifest } from '../src/manifest'
 import { r } from './utils'
 
-export function writeManifest() {
+function writeManifest() {
   fs.writeJSON(r('extension/manifest.json'), getManifest(), { spaces: 2 })
 }
 
